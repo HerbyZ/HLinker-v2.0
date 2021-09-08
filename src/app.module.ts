4968b71d3df84af941d8c907fcfbdeb2738f7804
HerbyZ <andreyherby@gmail.com>
2021-09-08 17:11:10 +0300

    Auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -15,7 +15,7 @@ import configuration from './config/configuration';
         uri: configService.get('database.uri'),
       }),
     }),
-    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
