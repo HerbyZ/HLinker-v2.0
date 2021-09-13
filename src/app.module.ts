1ba06a209e0d56874d90c8cc4ddb6031a398760b
HerbyZ <andreyherby@gmail.com>
2021-09-13 21:57:27 +0300

    Links module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { LinksModule } from './links/links.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -16,6 +17,7 @@ import configuration from './config/configuration';
       }),
     }),
     AuthModule,
+    LinksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
