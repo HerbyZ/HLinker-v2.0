8f64869d147643d4375ee70d54392c22e0cf6104
HerbyZ <andreyherby@gmail.com>
2021-10-10 22:10:53 +0300

    Home page styling

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './HomePage.scss';
 
 export const HomePage: React.FC = () => {
   return (
     <div className="home-page">
-      <h1>Hello, world!</h1>
+      <div className="container">
+        <div className="home-page-content">
+          <h1>HLinker v2</h1>
+          <h2>Portfolio project made in React with Typescript</h2>
+          <small className="text-secondary other-branches-hint">
+            Don't forget to check other branches on{' '}
+            <a href="https://github.com/HerbyZ/HLinker-v2.0">GitHub</a>
+          </small>
+          <br />
+          <a className="sign-up-link" href="/sign-up">
+            Sign up
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
